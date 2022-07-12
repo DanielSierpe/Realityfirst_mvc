af15d9809a379e9760c88353a69dbad865b954cb
DanielSierpe <daniel.sierpe.l@gmail.com>
2022-07-11 22:31:47 -0400

    cambios

---
+++ Realityfirst/Controllers/EventoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realityfirst.Controllers
+{
+    public class EventoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
